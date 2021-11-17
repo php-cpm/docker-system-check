b994818de7ec2588f85a31a25080589f0983a6d5
zouyi <php-cpm@users.noreply.github.com>
2021-11-17 12:10:06 +0800

    Update check_pkg.sh

--- check_pkg.sh
+++ check_pkg.sh
@@ -5,10 +5,10 @@ test -e /sbin/apk && is_apk=1 || is_apk=0
 test -e /usr/bin/apt && is_apt=1 || is_apt=0
 test -e /usr/bin/yum && is_yum=1 || is_yum=0
 
-if $is_yum == 1 :
+if [ $is_yum = 1 ]; then
   echo "yum"
-elif $is_apt == 1 :
+elif [ $is_apt = 1 ]; then
   echo "apt"
-elif $is_apk == 1 :
+elif [ $is_apk = 1 ]; then
   echo "apk"
 fi
