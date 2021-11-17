ab2c8f53a7e44ed15d7b5317c73d1a1c10375c71
zouyi <php-cpm@users.noreply.github.com>
2021-11-17 12:12:27 +0800

    Update check_os.sh

--- check_os.sh
+++ check_os.sh
@@ -2,6 +2,6 @@
 
 #refer https://www.freedesktop.org/software/systemd/man/os-release.html
 test -e /etc/os-release && os_release='/etc/os-release' || os_release='/usr/lib/os-release'
-. "${os_release}"
+
 OS=`grep '^ID=' ${os_release} |awk -F '=' '{print $2}' 2>/dev/null`
 echo ${OS}
