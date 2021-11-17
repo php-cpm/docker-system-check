847eca2504332cf200bd3c58a6c3a46c8b38c908
zouyi <php-cpm@users.noreply.github.com>
2021-11-17 12:18:25 +0800

    Create replace-debian-apt.sh

---
+++ replace-debian-apt.sh
@@ -0,0 +1,2 @@
+sed -i 's/deb.debian.org/mirrors.tuna.tsinghua.edu.cn/g' /etc/apt/sources.list
+sed -i 's|security.debian.org/debian-security|mirrors.tuna.tsinghua.edu.cn/debian-security|g' /etc/apt/sources.list
