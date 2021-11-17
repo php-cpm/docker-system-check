bc2ca6a4b7a1f2a6514e1c01f5fbc2e1757c522c
zouyi <php-cpm@users.noreply.github.com>
2021-11-17 12:14:14 +0800

    Create replace-centos-yum.sh

---
+++ replace-centos-yum.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#https://mirrors.tuna.tsinghua.edu.cn/help/centos/
+sed -e 's|^mirrorlist=|#mirrorlist=|g' \
+         -e 's|^#baseurl=http://mirror.centos.org|baseurl=https://mirrors.tuna.tsinghua.edu.cn|g' \
+         -i.bak \
+         /etc/yum.repos.d/CentOS-*.repo
